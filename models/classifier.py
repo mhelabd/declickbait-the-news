69081a0ee7c0c2a83ee7545c300eeef3e55ac93c
Michael Elabd <mhelabd@stanford.edu>
2022-03-10 23:41:23 -0800

    modified batchsize

--- models/classifier.py
+++ models/classifier.py
@@ -87,7 +87,7 @@ class ClassifierModule(nn.Module):
 
 if __name__ == "__main__":    
     train_dataset = TokenizedClickbaitDataset(TRAIN_PATH, load_dataset_path=TOKENIZED_DATASET_PATH_TRAIN, tokenizer= "bert")
-    train_dataloader = DataLoader(train_dataset, batch_size=1, save_model_batch=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=64, save_model_batch=True)
     if torch.cuda.is_available():
         device="cuda"
     else:
